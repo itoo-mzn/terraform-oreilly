31211bbb29bb168c85c60af3ed7b24b92a2f802e
Takuya Ito <ito0804takuya@gmail.com>
2026-01-08 07:57:01 +0900

    local変数で共通化

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -20,6 +20,17 @@ data "aws_subnets" "default" {
 #   }
 # }
 
+# ローカル変数（valiable.tfには入力変数を定義）
+locals {
+  http_port = 80
+  any_port  = 0
+
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+
+  all_ips = ["0.0.0.0/0"]
+}
+
 # ALB
 resource "aws_lb" "example" {
   name               = var.cluster_name
@@ -33,24 +44,24 @@ resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
   }
 }
 
 # ALBリスナー（受け付けるポートとプロトコルの設定）
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   default_action {
@@ -153,14 +164,14 @@ resource "aws_security_group" "instance" {
   ingress {
     from_port   = var.server_port
     to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
   }
 }
